c8f9b536916406f78cd64902f11e8d81f7507f45
vikashraj144 <vikashraj144@gmail.com>
2018-03-21 09:52:08 +0530

    added test

---
+++ test/test-pages.js
@@ -0,0 +1,9 @@
+var expect  = require('chai').expect;
+var request = require('request');
+
+it('Main page content', function(done) {
+    request('http://localhost:8080' , function(error, response, body) {
+        expect(body).to.equal('Hello World');
+        done();
+    });
+});
