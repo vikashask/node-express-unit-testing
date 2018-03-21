00893c4e731e17c16e583497af933b56766ff143
vikashraj144 <vikashraj144@gmail.com>
2018-03-21 09:55:53 +0530

    example with some more tests

--- test/test-pages.js
+++ test/test-pages.js
@@ -1,9 +1,33 @@
 var expect  = require('chai').expect;
 var request = require('request');
 
+/* it('Main page content', function(done) {
+    request('http://localhost:8080' , function(error, response, body) {
+        expect(body).to.equal('Hello World');
+        done();
+    });
+});
+ */
+
+// example with some more tests that will check the status
+
 it('Main page content', function(done) {
     request('http://localhost:8080' , function(error, response, body) {
         expect(body).to.equal('Hello World');
+        done();
+    });
+});
+
+it('Main page status', function(done) {
+    request('http://localhost:8080' , function(error, response, body) {
+        expect(response.statusCode).to.equal(200);
+        done();
+    });
+});
+
+it('About page content', function(done) {
+    request('http://localhost:8080/about' , function(error, response, body) {
+        expect(response.statusCode).to.equal(404);
         done();
     });
 });
