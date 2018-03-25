e606261816dd02fb91e36fa0921448171fd10bab
vikashraj144 <vikashraj144@gmail.com>
2018-03-25 12:28:49 +0530

    testing with test rout

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
+app.get('/test', function (req, res) {
+  res.send('Hello test')
+})
+
 //Launch listening server on port 8080
 app.listen(8080, function () {
   console.log('App listening on port 8080!')
--- test/test.js
+++ test/test.js
@@ -39,27 +39,29 @@ control your tests by grouping them:
 describe('Status and content', function() {
     describe ('Main page', function() {
         it('status', function(done){
-            request('http://localhost:8080/', function(error, response, body) {
+            request('http://localhost:8080/test', function(error, response, body) {
                 expect(response.statusCode).to.equal(200);
+                expect(response.body).to.equal('Hello test');
+                
                 done();
             });
         });
 
-        it('content', function(done) {
-            request('http://localhost:8080/' , function(error, response, body) {
-                expect(body).to.equal('Hello World');
-                done();
-            });
-        });
+        // it('content', function(done) {
+        //     request('http://localhost:8080/' , function(error, response, body) {
+        //         expect(body).to.equal('Hello World');
+        //         done();
+        //     });
+        // });
     });
 
-    describe ('About page', function() {
-        it('status', function(done){
-            request('http://localhost:8080/about', function(error, response, body) {
-                expect(response.statusCode).to.equal(404);
-                done();
-            });
-        });
+    // describe ('About page', function() {
+    //     it('status', function(done){
+    //         request('http://localhost:8080/about', function(error, response, body) {
+    //             expect(response.statusCode).to.equal(404);
+    //             done();
+    //         });
+    //     });
 
-    });
+    // });
 });
